8d7fb092da6ecd1637ce7ee4e53d46e3ce494e04
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-11-21 10:58:19 -0500

    added test

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+mod game;
+
+#[cfg(test)]
+mod tests {
+    use crate::game::player::*;
+
+    #[test]
+    fn player_initialization() {
+        let mut player = Player::new();
+        assert_eq!(player.get_points(), 100);
+        assert_eq!(player.get_deck_size(), 52 - 5);
+        assert_eq!(player.get_hand_size(), 5);
+        
+        player.draw_card();
+        assert_eq!(player.get_deck_size(), 52 - 6);
+        assert_eq!(player.get_hand_size(), 6);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use game::player::Player;
 fn main() {
     let player_one = Player::new();
     println!("Deck Size: {}", player_one.get_deck_size());
-    println!("Points: {}", player_one._get_points());
+    println!("Points: {}", player_one.get_points());
     player_one._print_hand();
 }
