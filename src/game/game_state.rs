deab8d66fe6857fe5eefa6f2b811ff6b49a15e4d
BlueHatDude <ehmdiagne0530@gmail.com>
2024-11-24 20:32:07 -0500

    fixed warning

--- src/game/game_state.rs
+++ src/game/game_state.rs
@@ -2,10 +2,11 @@ use super::player::Player;
 
 pub struct GameState {
     players: Vec<Player>,
-    current_player: u8,
+    current_player: usize,
 }
 
 impl GameState {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
             players: Vec::new(),
@@ -19,13 +20,17 @@ impl GameState {
 
     pub fn move_next_player(&mut self) {
         let num_players = self.players.len();
-        self.current_player = (self.current_player + 1) % (num_players as u8);
+        self.current_player = (self.current_player + 1) % (num_players);
+    }
+
+    pub fn get_current_player(&self) -> &Player {
+        self.players.get(self.current_player).unwrap()
     }
 
     pub fn _print_players(&self) {
         for player in self.players.iter() {
             player._print_self();
-            print!("\n");
+            println!();
         }
     }
 }
