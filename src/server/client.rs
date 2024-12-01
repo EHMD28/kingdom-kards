4780c5746ab40a1849c206cc847e0f9f194b9ac0
BlueHatDude <ehmdiagne0530@gmail.com>
2024-11-30 22:09:15 -0500

    added server module

---
+++ src/server/client.rs
@@ -0,0 +1,4 @@
+/// Returns a list of available Kingdom Kards servers.
+fn find_servers() {}
+
+fn connect() {}
---
+++ src/server/host.rs
@@ -0,0 +1,4 @@
+// use crate::game::game_state::GameState;
+
+/// Starts instance of Kingdom Kards server.
+fn start_server() {}
