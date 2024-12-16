ebad35be0a2f00f432afa015b6a5d66580e28a1c
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-12-16 11:41:42 -0500

    ran cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::io::{self, Write};
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
-
 use kingdom_kards::server::client::{
-    choose_player_name, connect_to_server, send_join_request, try_connect, ClientError,
+    choose_player_name, send_join_request, try_connect, ClientError,
 };
 use kingdom_kards::server::host::ServerInstance;
-use kingdom_kards::server::utils::{choose_mode, get_input, get_response, Mode};
+use kingdom_kards::server::utils::{choose_mode, Mode};
 use kingdom_kards::utils::clear_screen;
 
 fn main() {
@@ -39,37 +34,6 @@ fn main() {
             } else {
                 todo!()
             }
-
-            // if let Some(mut stream) = try_connect() {
-            //     loop {
-            //         match choose_player_name() {
-            //             Ok(name) => {
-            //                 let message = format!("JOIN,{name}");
-            //                 let _ = stream.write(message.as_bytes()).unwrap();
-            //                 thread::sleep(Duration::from_millis(500));
-
-            //                 // LEFT OFF HERE
-            //                 let response = get_response(&mut stream);
-
-            //                 // name was rejected
-            //                 if response == "REJECT" {
-            //                     continue;
-            //                 } else {
-            //                     println!("Sucessfully joined server");
-            //                 }
-
-            //                 break;
-            //             }
-            //             Err(err) => match err {
-            //                 ClientError::InvalidCharacterFound => {
-            //                     println!(
-            //                         "Name cannot contain special characters (e.g. ç or emoji)"
-            //                     );
-            //                 }
-            //                 ClientError::CommaFound => println!("Name cannot contain comma"),
-            //             },
-            //         }
-            //     }
         }
     }
 }
