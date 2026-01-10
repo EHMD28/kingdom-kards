320b406e96ddb0bbce6231c53b97cd254695ff2e
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-09 21:47:45 -0500

    deleted everything

--- src/game/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod card;
-pub mod game_state;
-pub mod player;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-use kingdom_kards::server::client::ClientInstance;
-use kingdom_kards::server::host::ServerInstance;
-use kingdom_kards::server::utils::{choose_mode, Mode};
-use kingdom_kards::utils::clear_screen;
-
 fn main() {
-    clear_screen();
-    println!("Starting Kingdom Kards...\n");
-
-    let mode = choose_mode();
-
-    match mode {
-        Mode::HostGame => {
-            let mut server = ServerInstance::create();
-            server.start();
-            server._wait();
-        }
-        Mode::ConnectGame => {
-            let mut client = ClientInstance::new();
-
-            if client.connect_to_server("127.0.0.1:5464").is_none() {
-                return; /* close application */
-            }
-
-            client.start();
-            client._wait();
-        }
-    }
+    println!("Hello, world!");
 }
